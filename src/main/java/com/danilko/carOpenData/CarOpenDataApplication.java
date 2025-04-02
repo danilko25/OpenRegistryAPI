6dd8a8d03f1d6760c0abffabc5e4acd5035d5e9f
Danil <danil.r.savchenko@gmail.com>
2025-04-03 00:13:24 +0300

    CarOpenData project initialization

---
+++ src/main/java/com/danilko/carOpenData/CarOpenDataApplication.java
@@ -0,0 +1,12 @@
+package com.danilko.carOpenData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarOpenDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarOpenDataApplication.class, args);
+    }
+}
